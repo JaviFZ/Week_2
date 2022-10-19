226690b825a3f36fd5845d60e5d3f28fc2e02fb1
JaviFZ <jferzap@gmail.com>
2022-10-19 17:45:49 +0200

    subiendo reto1

--- POO/clasePerson.js
+++ POO/clasePerson.js
@@ -1,1 +1,12 @@
 
+class Person {
+    constructor( uniforme, hat ){
+        this.alura
+        this.peso
+        this.colorPelo
+        this.colorOjos
+        this.uniforme = uniforme
+        this.hat = hat
+    }
+}
+
