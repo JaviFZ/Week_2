a25fbda0063c2f97f3b4f2019983bb0beecbf3dd
JaviFZ <jferzap@gmail.com>
2022-10-24 12:11:52 +0200

    Subida de actualizaciones

--- typeScript/vector.ts
+++ typeScript/vector.ts
@@ -64,58 +64,54 @@ export class Vector {
 
 // // Realiza la resta de elements con v1:
 
-    public subs(v1:Vector) : Vector {
-        let vectorRestado : number[] = [];
-        for (let i = 0; i < this.elements.length; i++){
-            let result = this.elements[i] - v1.getElements()[i];
-            vectorRestado.push(result);
-        }
-        let vector = new Vector(0,0);
-        vector.setElements(vectorRestado)
-        return vector;
+public subs(v1:Vector) : Vector {
+    let vector : Vector = new Vector(0,0);
+    for (let i = 0; i < this.elements.length; i++){
+        vector.elements.push(this.elements[i] - v1.elements[i]);
     }
+    return vector;
+}
 
 
 // // Realiza el producto de elements con v1:
 
-    public mult(v1:Vector) : Vector {
-        let vectorMult : number[] = [];
-        for (let i = 0; i < this.elements.length; i++){
-            let result = this.elements[i] *  v1.getElements()[i];
-            vectorMult.push(result);
-            
-        }
-        let vector = new Vector(0,0);
-        vector.setElements(vectorMult)
-        return vector;
+public mult(v1:Vector) : Vector {
+    let vector : Vector = new Vector(0,0);
+    for (let i = 0; i < this.elements.length; i++){
+        vector.elements.push(this.elements[i] * v1.elements[i]);
     }
+    return vector;
+}
 
-    // public multa(v1:Vector) : void {
-    //     let vectorMulti : number[] = [];
-    //     for (let i = 0; i < this.elements.length; i++){
-    //         // if (this.elements.length == v1.getElements.length){
-    //         let result = this.elements[i] * v1.getElements()[i];
-    //         vectorMulti.push(result);
-    //         // }
-    //     }
-    //     this.elements = vectorMulti;
-    // }
+   
 
 
 
 
     // // Realiza el producto entre elements y el número n:
 
+
+
     public multNumber(n : number) : Vector {
-        let vectorProd : number[] = [];
+        let vector : Vector = new Vector(0,0);
         for (let i = 0; i < this.elements.length; i++){
-            let result = this.elements[i] * n;
-            vectorProd.push(result);
+            vector.elements.push(this.elements[i] * n);
         }
-        let vector = new Vector(0,0)
-        vector.setElements(vectorProd)
-        return vector
+        return vector;
     }
+
+
+
+    // public multNumber(n : number) : Vector {
+    //     let vectorProd : number[] = [];
+    //     for (let i = 0; i < this.elements.length; i++){
+    //         let result = this.elements[i] * n;
+    //         vectorProd.push(result);
+    //     }
+    //     let vector = new Vector(0,0)
+    //     vector.setElements(vectorProd)
+    //     return vector
+    // }
 }      
 
 
